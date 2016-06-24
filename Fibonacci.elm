98f5018bb7dbce3743bee6bab40b12e799b4c22f
FERNANDO VITON <fer@FERNANDOs-MacBook-Pro.local>
2016-06-23 22:14:02 -0700

    A better version

--- Fibonacci.elm
+++ Fibonacci.elm
@@ -1,8 +1,8 @@
 import Html exposing (text)
 
-fib curFib maxLength =
-  if (List.length curFib) >= maxLength then curFib
-  else (fib (curFib ++ [(List.sum (List.drop ((List.length curFib) - 2) curFib))]) maxLength)
+fib curFib lengthToAppend =
+  if lengthToAppend <= 0 then curFib
+  else (fib (curFib ++ [(List.sum (List.drop ((List.length curFib) - 2) curFib))]) (lengthToAppend - 1))
 
 main =
   text (toString (fib [0, 1] 100))
