83eb880b682065b8815fb8d723e5964740ebaf80
Fernndo Viton <fcode@viton.org>
2016-06-28 00:11:41 +0200

    Refactor

--- src/Fibonacci.elm
+++ src/Fibonacci.elm
@@ -5,11 +5,20 @@ import Html exposing (text)
 fibonacci length =
   if length == 0 then []
   else if length == 1 then [1]
-  else fib [1, 1] (length - 2)
+  else fib [1, 1] 1 1 (length - 2)
 
-fib curFib lengthToAppend =
+
+fib curFib prev last lengthToAppend =
   if lengthToAppend <= 0 then curFib
-  else (fib (curFib ++ [(List.sum (List.drop ((List.length curFib) - 2) curFib))]) (lengthToAppend - 1))
+  else
+    let
+      next =
+        prev + last
+      newLength =
+        lengthToAppend - 1
+      in
+        (fib (curFib ++ [next]) last next newLength)
+
 
 main =
   text (toString (fibonacci 100))
