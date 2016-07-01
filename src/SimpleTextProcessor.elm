0c0495290c5b38b9b8dcad322e214123ec2bb3a8
Fernndo Viton <fcode@viton.org>
2016-07-01 17:11:48 +0200

    some copied code

--- src/SimpleTextProcessor.elm
+++ src/SimpleTextProcessor.elm
@@ -1,4 +1,32 @@
-import Html exposing (text)
+import Html exposing (div)
+import Html.App
+import Html.Attributes exposing (contenteditable, property, style)
+import Html.Events exposing (on)
+import Json.Decode exposing (at, string)
+import Json.Encode
 
 main =
-  text "Hello, World!"
+  Html.App.beginnerProgram
+  { model = ""
+  , update = update
+  , view = view
+  }
+
+type Msg = Input String
+
+update (Input newModel) model =
+  newModel
+
+view model =
+  div []
+    [ div
+      [ contenteditable True
+      , on "input" (Json.Decode.map Input innerHtmlDecoder)
+      , "height" => "200px"
+      , style [("width", "100%"), ("border", "1px solid green")]
+      ] []
+    , div [ property "innerHTML" (Json.Encode.string model) ] []
+    ]
+
+innerHtmlDecoder =
+  at ["target", "innerHTML"] string
